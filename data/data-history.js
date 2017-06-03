cf79eba82c9d75a105ed800df3fee28ed26a0158
jz13686841147 <846333248@qq.com>
2017-06-03 17:40:36 +0800

    xiugaifenye

---
+++ data/data-history.js
@@ -0,0 +1,325 @@
+data = {
+    "data":[
+        {
+            "con":[
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    // "title":"第1页 第1条数据 标题",
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                }
+            ]
+        },
+        {
+            "con":  [
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                }
+            ]
+        },
+        {
+            "con":[
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                }
+            ]
+        },
+        {
+            "con":[
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                }
+            ]
+        },
+        {
+            "con":[
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                },
+                {
+                    "serial":"84562135412065412",
+                    "card_number":"0000000021012101",
+                    "start_time":"2017-05-18/12:20:12",
+                    "end_time":"2017-05-18/14:20:12",
+                    "duration":"2小时",
+                    "electric": "307KW",
+                    "money": "¥111",
+                    "actual_money":"100",
+                    "chargerID":"LS0012",
+                    "balance": "¥1111"
+                }
+            ]
+        }
+    ]
+}
